443dd9aab144fdfe8c5b853ada8c60d1274e6e92
Toxiccan <akash.musham@gmail.com>
2026-07-27 17:34:54 +0530

    Time: 1 ms (99.94%), Space: 94.5 MB (33.25%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int maxprofit = 0;
+        while(r < prices.length)
+        {
+            if(prices[r] > prices[l])
+            {
+                int profit = prices[r] - prices[l];
+                maxprofit = Math.max(maxprofit,profit);
+            }
+            else
+            {
+                l = r;
+            }
+            r++;
+        }
+        return maxprofit;
+    }
+}
