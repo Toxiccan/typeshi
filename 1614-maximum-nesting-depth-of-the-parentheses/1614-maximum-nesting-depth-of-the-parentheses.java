fa0bc721627980f78d740999232938dfeaddb7ec
Toxiccan <akash.musham@gmail.com>
2026-09-10 14:50:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (70.3%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0;
+        int max_depth = 0;
+        for(int i = 0;i < s.length();i++)
+        {
+            if(s.charAt(i) == '(')
+            {
+                depth++;
+            }
+            else if(s.charAt(i) == ')')
+            {
+                depth--;
+            }
+            else
+            {
+                continue;
+            }
+            max_depth = Math.max(depth,max_depth);
+        }
+        return max_depth;
+    }
+}
